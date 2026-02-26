1a7b7e34ca422b080f3c7a4daf44fbe8906fcec3
Nishank-C-Jain <nishankjain.study@gmail.com>
2026-02-26 12:30:33 +0530

    minor changes

---
+++ project/frontend/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyArmp7LtzuH4GKXUX9xKKMgaMHV1ioSeKU",
+  authDomain: "realestate-ba037.firebaseapp.com",
+  projectId: "realestate-ba037",
+  storageBucket: "realestate-ba037.firebasestorage.app",
+  messagingSenderId: "1014619180710",
+  appId: "1:1014619180710:web:49f231abc721762a472e67",
+  measurementId: "G-YS2N7C27EC"
+};
