b658f456f00973bfb2cc7cea6e257d2e7a838813
Nishank-C-Jain <nishankjain.study@gmail.com>
2026-03-15 18:36:19 +0530

    edited the file for make the project working on live link..

--- project/frontend/config.js
+++ project/frontend/config.js
@@ -1,7 +1,7 @@
 // Centralized API Configuration
 
 // Production URL (Commented out for testing new email logic)
-// export const API_BASE_URL = "https://project2-mj7h.onrender.com";
+export const API_BASE_URL = "https://project2-mj7h.onrender.com";
 
 // Localhost URL (Active for local development)
-export const API_BASE_URL = "http://localhost:3000";
+// export const API_BASE_URL = "http://localhost:3000";
